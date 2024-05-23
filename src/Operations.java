8f9802beed21b971dce92ad62dfdb0ab9babc0c1
Support Itstars <support@itstars.co.tz>
2024-05-23 07:51:59 +0300

    My first Commit

---
+++ src/Operations.java
@@ -0,0 +1,18 @@
+public class Operations {
+    class Trial{
+        void fly(int distance) {
+            
+        }
+        int fly(int time, int speed) {return time*speed;}
+
+        void glide(int time) {
+            System.out.println("glide test");
+        }
+        void Glide(int time) {
+            System.out.println("Glide senior");
+        }
+    }
+    public static void main(String[] args) {
+        System.out.println("https://youtu.be/Ae-r8hsbPUo?si=t1y0Ml0-PXMEkvsX");
+    }
+}
